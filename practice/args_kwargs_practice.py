21ab9c261ae6acb3cee501567c0a4279d0a2c7b8
Yadwinder Singh Bhullar <ysbhullar31@gmail.com>
2026-08-25 13:33:55 -0400

    Add args and kwargs practice

--- practice/args_kwargs_practice.py
+++ practice/args_kwargs_practice.py
@@ -1,7 +1,9 @@
 # *args and **kwargs Practice
 
 
+# ==========================================
 # 1. *args
+# ==========================================
 
 def add_numbers(*numbers):
     return sum(numbers)
@@ -13,10 +15,14 @@ print(add_numbers(10, 20, 30))
 print(add_numbers(10, 20, 30, 40))
 
 
+# ==========================================
 # 2. **kwargs
+# ==========================================
 
 def employee_info(**details):
-    print(details)
+
+    for key, value in details.items():
+        print(f"{key}: {value}")
 
 
 print()
@@ -26,4 +32,33 @@ employee_info(
     name="John",
     salary=5000,
     department="Sales"
+)
+
+
+# ==========================================
+# 3. *args + **kwargs
+# ==========================================
+
+def employee_data(*skills, **details):
+
+    print()
+    print("SKILLS")
+
+    for skill in skills:
+        print(skill)
+
+    print()
+    print("EMPLOYEE DETAILS")
+
+    for key, value in details.items():
+        print(f"{key}: {value}")
+
+
+employee_data(
+    "Python",
+    "SQL",
+    "Pandas",
+    name="Sarah",
+    salary=7000,
+    department="Data Analysis"
 )
