6807cdfed22ab212e9222ded2dd3abee8a41805c
Yadwinder Singh Bhullar <ysbhullar31@gmail.com>
2026-09-05 15:59:44 -0400

    Practice inheritance and polymorphism

--- practice/oop/inheritance_practice.py
+++ practice/oop/inheritance_practice.py
@@ -7,6 +7,11 @@ class Employee:
         print(f"Name: {self.name}")
         print(f"Salary: ${self.salary}")
 
+    def calculate_bonus(self):
+        return self.salary * 0.10
+        
+
+
 
 class Manager(Employee):
 
@@ -18,11 +23,40 @@ class Manager(Employee):
         super().display_info()
         print(f"Department: {self.department}")
 
+    def calculate_bonus(self):
+        return self.salary * 0.20
+
+
+class Developer(Employee):
+    def __init__(self, name , salary, programming_language):
+        super().__init__(name,salary)
+        self.programming_language = programming_language
+
+    def display_info(self):
+        super().display_info()
+        print(f"Programming Language: {self.programming_language}")
+
+    def calculate_bonus(self):
+        return self.salary * 0.15
+
+
+
 
 employee1 = Employee("John", 50000)
 manager1 = Manager("Sarah", 80000, "Sales")
+developer1 = Developer("Mike",70000, "python")
 
-employee1.display_info()
-print()
 
-manager1.display_info()
+
+
+
+employees = [employee1, manager1, developer1]
+for employee in employees:
+    employee.display_info()
+    print(f"Bonus: ${employee.calculate_bonus()}")
+    print()
+
+
+
+
+
